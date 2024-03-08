502c88ac3467aadfc7a52d3e243688ccf034c499
Mark <qudfbftnqls@naver.com>
2024-03-09 00:04:40 +0900

    AnswerRepository 이용한 CRUD 테스트

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -3,6 +3,7 @@ package com.mysite.sbb;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -16,6 +17,9 @@ class SbbApplicationTests {
 
     @Autowired
     private QuestionRepository questionRepository;
+
+    @Autowired
+    private AnswerRepository answerRepository;
 
     @Test
     void saveData() {
@@ -87,5 +91,39 @@ class SbbApplicationTests {
         Question q = oq.get();
         this.questionRepository.delete(q);
         assertEquals(1, this.questionRepository.count());
+    }
+
+    @Test
+    void saveAnswer() {
+        Optional<Question> oq = this.questionRepository.findById(12);
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
+
+        Answer a = new Answer();
+        a.setContent("네 자동으로 생성됩니다.");
+        a.setQuestion(q); // 어떤 질문의 답변인지 알기 위해서 Question 객체가 필요하다.
+        a.setCreateDate(LocalDateTime.now());
+        this.answerRepository.save(a);
+    }
+
+    @Test
+    void findAnswerById() {
+        Optional<Answer> oa = this.answerRepository.findById(1);
+        assertTrue(oa.isPresent());
+        Answer a = oa.get();
+        assertEquals(12, a.getQuestion().getId());
+    }
+
+    @Transactional
+    @Test
+    void findAnswerByQuestionId() {
+        Optional<Question> oq = this.questionRepository.findById(12);
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
+
+        List<Answer> answerList = q.getAnswerList();
+
+        assertEquals(1, answerList.size());
+        assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
     }
 }
